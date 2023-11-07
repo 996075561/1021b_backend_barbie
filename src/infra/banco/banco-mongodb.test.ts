ad676396cee16653ae061eda969fa8fa30edb417
996075561 <luana.trindade@estudante.ifms.edu.br>
2023-11-07 07:58:31 -0400

    commit

---
+++ src/infra/banco/banco-mongodb.test.ts
@@ -0,0 +1,11 @@
+import {test, expect, describe} from 'vitest'
+
+import { BancoMongoDB, BancoMongoDB } from './banco-mongodb'
+
+describe('BancoMongoDB test', () => {
+test ('BancoMongoDB', () => {
+    const BancoMongoDB = new BancoMongoDB()
+    expect(bancoMongoDB).toBeDefined()
+})
+test('deves salvar um filme', )
+})
---
+++ src/infra/banco/banco-mongodb.ts
@@ -0,0 +1,3 @@
+export class BancoMongoDB implements FilmeRepositorioInterface {
+
+}
