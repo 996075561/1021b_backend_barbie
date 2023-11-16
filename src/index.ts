ba45185f9956040a8c382dc7dfdb50e11737b8f2
996075561 <luana.trindade@estudante.ifms.edu.br>
2023-11-16 10:19:21 -0400

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import express from 'express'
 const app = express()
 app.use(express.json())
+import SalvaFilme from './infra/banco/banco-mongodb'
 import ListaFilme from './aplicacao/lista-filme.use-case'
 import BancoMongoDB from './infra/banco/banco-mongodb'
 const bancoMongoDB = new BancoMongoDB()
 app.get('/filmes',async (req,res)=>{
     //usem o listarFilme Usecase para listar os filmes
-    
     const listaFilme = new ListaFilme(bancoMongoDB)
     const filmes = await listaFilme.executar()
     res.send(filmes)
+})
+app.post('/filmes',async(req,res)=>{
+    const {id,titulo,descricao,imagem} = req.body
+    const filme = {
+        id,
+        titulo,
+        descricao,
+        imagem
+    }
+    const SalvaFilme = new SalvaFilme(bancoMongoDB)
+    const resposta = await SalvaFilme.execute(filme)
+    res.status(201).send(filme)
+  
 })
 app.post('/filmes',(req,res)=>{
     const {id,titulo,descricao,imagem} = req.body
