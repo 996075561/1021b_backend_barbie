1080f376a1cca7f5a61080fc63fe2517e655ba01
996075561 <luana.trindade@estudante.ifms.edu.br>
2023-11-17 08:18:16 -0400

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,73 @@
-import express from 'express'
-const app = express()
-app.use(express.json())
-import SalvaFilme from './aplicacao/salva-filme.use-case'
-import ListaFilme from './aplicacao/lista-filme.use-case'
-import BancoMongoDB from './infra/banco/banco-mongodb'
-const bancoMongoDB = new BancoMongoDB()
-app.get('/filmes',async (req,res)=>{
-    //usem o listarFilme Usecase para listar os filmes
-    const listaFilme = new ListaFilme(bancoMongoDB)
-    const filmes = await listaFilme.executar()
-    res.send(filmes)
-})
-app.post('/filmes',async(req,res)=>{
-    const {id,titulo,descricao,imagem} = req.body
-    const filme = {
-        id,
-        titulo,
-        descricao,
-        imagem
-    }
-    const SalvaFilme = new SalvaFilme(bancoMongoDB)
-    const resposta = await SalvaFilme.execute(filme)
-    res.status(201).send(filme)
+import express from 'express';
+import ListaFilme from './aplicacao/lista-filme.use-case';
+import BancoMongoDB from './infra/banco/banco-mongodb';
+import SalvaFilme from './aplicacao/salva-filme.use-case';
+
+const app = express();
+app.use(express.json());
+
+const bancoMongoDB = new BancoMongoDB();
+
+app.get('/filmes', async (req, res) => {
+  try {
+    const listaFilme = new ListaFilme(bancoMongoDB);
+    const filmes = await listaFilme.executar();
+    res.send(filmes);
+  } catch (error) {
+    res.status(500).send('Erro ao buscar filmes');
+  }
+});
+
+app.post('/filmes', async (req, res) => {
   
-})
-app.post('/filmes',(req,res)=>{
-    const {id,titulo,descricao,imagem} = req.body
+    const { id, titulo, descricao, imagem } = req.body;
     const filme = {
-        id,
-        titulo,
-        descricao,
-        imagem
+      id,
+      titulo,
+      descricao,
+      imagem
+    };
+    try {
+    const salvaFilme = new SalvaFilme(bancoMongoDB);
+    await salvaFilme.execute(filme);
+    res.status(201).send(filme);
     }
-    //Como eu salvo o filme que foi cadastrado no meu vetor de filmes (Banco de dados)
-    filmesCadastros.push(filme)
-    res.status(201).send(filme)
-})
+    catch (error) {
+    res.status(500).send('Erro ao cadastrar o filme');
+  }
+});
 
-app.get('/filmes/:id',(req,res)=>{
-    const id = parseInt(req.params.id)
-    //FIND para buscar um filme pelo id e retornar para o usuário
-    const filme = filmesCadastros.find(filme => filme.id === id)
-    if(!filme) return res.status(404).send("Filme não encontrado")
-    res.status(200).send(filme)
-})
+app.get('/filmes/:id', (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    
+    const filme = buscarFilmePorId(id); 
 
-//Tenho que iniciar o servidor na porta 3000
-app.listen(3000,()=>{
-    console.log('Servidor rodando na porta 3000')
-})
+    if (!filme) {
+      return res.status(404).send('Filme não encontrado');
+    }
+    res.status(200).send(filme);
+  } catch (error) {
+    res.status(500).send('Erro ao buscar o filme');
+  }
+});
 
-//Tenho que ter uma rota post para cadastrar um filme
 
-//Salvar em algum lugar o filme que foi cadastrado.
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
+
+
 type Filme = {
-    id:number,
-    titulo:string,
-    descricao:string,
-    imagem:string
+  id: number;
+  titulo: string;
+  descricao: string;
+  imagem: string;
+};
+
+let filmesCadastrados: Filme[] = []; 
+
+
+function buscarFilmePorId(id: number): Filme | undefined {
+  return filmesCadastrados.find(filme => filme.id === id);
 }
-let filmesCadastros:Filme[] = []
